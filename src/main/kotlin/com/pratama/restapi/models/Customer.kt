33093da703d4d0832a77e4d0f8cbda79b032394c
pratamawijaya <set.mnemonix@gmail.com>
2022-05-31 23:58:01 +0700

    wip

--- src/main/kotlin/com/pratama/restapi/models/Customer.kt
+++ src/main/kotlin/com/pratama/restapi/models/Customer.kt
@@ -3,4 +3,4 @@ package com.pratama.restapi.models
 import org.springframework.data.relational.core.mapping.Table
 
 @Table("customer")
-data class Customer(val id: String, val firstName: String, val lastName: String, val email: String)
+data class Customer(val id: Int, val firstName: String, val lastName: String, val email: String)
--- src/main/resources/sql/schema.sql
+++ src/main/resources/sql/schema.sql
@@ -1,6 +1,6 @@
-CREATE TABLE IF NOT EXISTS customer {
-        id VARCHAR(60) PRIMARY KEY,
+CREATE TABLE IF NOT EXISTS customer (
+        id int not null AUTO_INCREMENT PRIMARY KEY,
         first_name varchar(100),
         last_name varchar(100),
         email varchar(20)
-    }
+);
